e6a87f810269ef0927ffc2efc524478b20028151
Rainer Sawitzki <training@rainer-sawitzki.de>
2017-05-16 13:24:40 +0200

    Added JMX

--- org.javacream.training.docker.si/src/main/java/org/javacream/training/docker/si/Demo.java
+++ org.javacream.training.docker.si/src/main/java/org/javacream/training/docker/si/Demo.java
@@ -1,8 +1,15 @@
 package org.javacream.training.docker.si;
 
+import java.util.Date;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 public class Demo {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception{
 		System.out.println(new Demo().doSomething());
+		ScheduledExecutorService executorService = Executors.newScheduledThreadPool(1);
+		executorService.scheduleAtFixedRate(()-> {System.out.println("Executing at " + new Date());System.gc();} , 0, 5, TimeUnit.SECONDS);
 	}
 
 	public String doSomething() {
---
+++ org.javacream.training.docker.si_noparent/src/main/java/org/javacream/training/docker/si/FileTransferUtilMBean.java
@@ -0,0 +1,8 @@
+package org.javacream.training.docker.si;
+
+public interface FileTransferUtilMBean {
+
+	int getIterations();
+	int getFilesDeleted();
+	void reset();
+}
